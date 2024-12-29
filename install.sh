aded94cdb1ade377b9cbb4f02d4b65f0c3a2333a
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2024-12-29 22:28:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ echo -e "${GREEN}======================== NodeJS, MongoDB, GenieACS, ===========
 echo -e "${GREEN}========================= . Info 081-947-215-703 ===========================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
-echo -e "${GREEN}${NC}"
 echo -e "${GREEN}=============================================================================${NC}"
 echo -e "${RED}${NC}"
 echo -e "${GREEN}Sebelum melanjutkan, silahkan baca terlebih dahulu. Apakah anda ingin melanjutkan? (y/n)${NC}"

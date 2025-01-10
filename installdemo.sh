1e2627105f564f617b35d09917b504bb33824c52
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2025-01-10 15:51:01 +0700

    Update installdemo.sh

--- installdemo.sh
+++ installdemo.sh
@@ -235,4 +235,4 @@ echo -e "${GREEN}=================== VIRTUAL PARAMETER BERHASIL DI INSTALL. ====
 echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
 echo -e "${GREEN}=================== Informasi: Whatsapp 081947215703 =======================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
-sudo rm -r genieacs
+sudo rm -r /genieacs

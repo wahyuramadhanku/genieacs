553038ca628f89935fbda342de3e5cd6a162cc74
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2025-01-11 10:17:07 +0700

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -236,4 +236,4 @@ echo -e "${GREEN}===Jika ACS URL berbeda, silahkan edit di Admin >> Provosions >
 echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
 echo -e "${GREEN}=================== Informasi: Whatsapp 081947215703 =======================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
-sudo rm -r /genieacs
+sudo rm -r genieacs

6805a1d229c1e09bd002b81bcecaf73899c6aebf
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2025-01-11 20:43:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -205,7 +205,8 @@ for ((i = 5; i >= 1; i--)); do
     echo "Lanjut Install Parameter $i. Tekan ctrl+c untuk membatalkan"
 done
 
-cd -
+cd
+sudo mongodump --db=genieacs --out genieacs-backup
 sudo mongorestore --db=genieacs --drop genieacs
 #Sukses
 echo -e "${GREEN}============================================================================${NC}"

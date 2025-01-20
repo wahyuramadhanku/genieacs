ae3290d7c8031f14724962569f664107f81ed37e
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2025-01-20 22:06:27 +0700

    Update darkmode.sh

--- darkmode.sh
+++ darkmode.sh
@@ -15,10 +15,11 @@ echo -e "${GREEN}===============================================================
 echo -e "${GREEN}========================= . Info 081-947-215-703 ===========================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}============================== WARNING!!! ==================================${NC}"
+echo -e "${GREEN}${NC}"
 echo -e "${GREEN}CONFIG DEMO VERSION${NC}"
 echo -e "${GREEN}Autoinstall GenieACS.${NC}"
 echo -e "${GREEN}${NC}"
-echo -e "${GREEN}======================================================================================${NC}"
+echo -e "${GREEN}=============================================================================${NC}"
 echo -e "${RED}${NC}"
 echo -e "${GREEN}Sebelum melanjutkan, silahkan baca terlebih dahulu. Apakah anda ingin melanjutkan? (y/n)${NC}"
 read confirmation
